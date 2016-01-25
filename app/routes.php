ff3a27898ff285919361ba5c18cca130fda0c06a
ton1o1 <ton1o1@users.noreply.github.com>
2016-01-25 14:29:39 +0100

    Routes update

--- app/routes.php
+++ app/routes.php
@@ -5,4 +5,6 @@
 		['GET', '/quiz/[i:quizId]?', 'Quiz#view', 'quiz_view'],
 		['GET', '/quiz/user/[i:userId]', 'Quiz#viewByUser', 'quiz_view_user'],
 		['GET|POST', '/quiz/create', 'Quiz#create', 'quiz_create'],
+		['GET|POST', '/quiz/edit/[i:quizId]', 'Quiz#edit', 'quiz_edit'],
+		['GET|POST', '/quiz/delete/[i:quizId]', 'Quiz#delete', 'quiz_delete'],
 	);
